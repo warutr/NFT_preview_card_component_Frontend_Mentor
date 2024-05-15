0d9b47ad3e0722a80e99bcfc391756289c7d6a7e
warut_r <74519790+warutr@users.noreply.github.com>
2024-05-15 21:24:26 +0700

    new project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,16 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      "soft-blue": "hsl(215, 51%, 70%)",
+      cyan: "hsl(178, 100%, 50%)",
+      "very-dark-blue": {
+        DEFAULT: "hsl(217, 54%, 11%)",
+        card: "hsl(216, 50%, 16%)",
+        line: "hsl(215, 32%, 27%)",
+      },
+      white: "hsl(0, 0%, 100%)",
+    },
   },
   plugins: [],
 };
